f27a8a2052dbeb08007ab22802e3496a89eb1f53
Vincenc <vincencliao@gmail.com>
2020-03-18 23:15:25 +0800

    add q300  Longest Increasing Subsequence

---
+++ algorithm-csharp/LeetCode/DP/LongestIncreasingSubsequence.cs
@@ -0,0 +1,46 @@
+﻿/* https://leetcode.com/problems/longest-increasing-subsequence
+ * 
+ * Runtime: 100 ms, faster than 84.46% of C# online submissions for Longest Increasing Subsequence.
+ * Memory Usage: 24 MB, less than 25.00% of C# online submissions for Longest Increasing Subsequence.
+ * 
+ * use DP to solve this question
+ * 
+ * time O(n^2)
+ * space O(n)
+ * 
+ */
+using System;
+
+namespace algorithm_csharp
+{
+    class LongestIncreasingSubsequence
+    {
+        public int LengthOfLIS(int[] nums)
+        {
+            int _length = nums == null ? 0 : nums.Length;
+            if (_length == 0) return 0;
+
+            int _maxLength = 0;
+
+            int[] dp = new int[_length];
+            dp[0] = 1;
+            //10, 9, 2, 5, 3, 7, 101, 18
+            //1, 3, 6, 7, 9, 4, 10, 5, 6
+            for (int tail = 0; tail < _length; tail++)
+            {
+                int currentMax = 0;
+                for (int current = 0; current < tail; current++)
+                {
+                    if (nums[tail] > nums[current])
+                    {
+                        currentMax = Math.Max(currentMax, dp[current]);
+                    }
+                }
+                dp[tail] = currentMax + 1;
+                _maxLength = Math.Max(_maxLength, dp[tail]);
+            }
+
+            return _maxLength;
+        }
+    }
+}
--- algorithm-csharp/Program.cs
+++ algorithm-csharp/Program.cs
@@ -10,16 +10,10 @@ namespace algorithm_csharp
     {
         static void Main(string[] args)
         {
-            MaximumSubarray test = new MaximumSubarray();
-            Console.WriteLine(test.MaxSubArray(new Int32[] { }));
-            Console.WriteLine(test.MaxSubArray(null));
-            Console.WriteLine(test.MaxSubArray(new Int32[] { -2, 1, -3, 4, -1 }));
-            Console.WriteLine(test.MaxSubArray(new Int32[] { 2, 2, -3, 4, -1 }));
-            Console.WriteLine(test.MaxSubArray(new Int32[] { -1, 2, -3, 4, -1 }));
-            Console.WriteLine(test.MaxSubArray(new Int32[] { -1, -2, -3, -4, -1 }));
-            Console.WriteLine(test.MaxSubArray(new Int32[] { -1, -2, -3, 0, -1 }));
-            Console.WriteLine(test.MaxSubArray(new Int32[] { -1, -2, 9, 0, -1 }));
-            Console.WriteLine(test.MaxSubArray(new Int32[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+            LongestIncreasingSubsequence test = new LongestIncreasingSubsequence();
+            Console.WriteLine(test.LengthOfLIS(new Int32[] { }));
+            Console.WriteLine(test.LengthOfLIS(null));
+            Console.WriteLine(test.LengthOfLIS(new Int32[] { 1, 3, 6, 7, 9, 4, 10, 5, 6 }));
 
 
             Console.ReadKey();
