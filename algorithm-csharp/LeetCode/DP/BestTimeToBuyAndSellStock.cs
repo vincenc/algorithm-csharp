5433aa993ea533208f510cdf72978302fd96347f
文成 廖 <vincencliao@gmail.com>
2020-03-18 17:37:13 +0800

    add q121 best buy and sell

---
+++ algorithm-csharp/LeetCode/DP/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,48 @@
+﻿// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/
+//
+// Runtime: 92 ms, faster than 87.08% of C# online submissions for Best Time to Buy and Sell Stock.
+// Memory Usage: 25.5 MB, less than 9.52% of C# online submissions for Best Time to Buy and Sell Stock.
+//
+//
+// use dynamic programming to get max result from start and reuse those result for later 
+// f1 => sell   => f1(x) = max(f1(x-1), f2(x-1) + todayPrice)
+// f2 => not sell cost => f2(x) = max( f2(x-1) , todayPrice * -1)
+
+
+using System;
+
+namespace algorithm_csharp
+{
+    class BestTimeToBuyAndSellStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+            int _length = prices == null ? 0 : prices.Length;
+            if (_length == 0) return maxProfit;
+
+            // 7,1,5,3,6,4
+            // f1 = max sell profit => f1(0) = 0 , f1(1) = 0 , f1(2) = 4 , f1(3) = max(4, f2(2) + 3)
+            // f2 = min not sell profit => f2(0) = -7 , f2(1) = -1 , f2(2) = -1 , f2(3) = max( last buy not sell, this time buy)
+            // can not buy twice => f2 will be current min cost
+
+            int[] f1 = new int[_length]; // sell profit
+            int[] f2 = new int[_length]; // not sell profit
+
+            for (int i = 0; i < _length; i++)
+            {
+                if (i == 0)
+                {
+                    f1[0] = 0;
+                    f2[0] = -1 * prices[0];
+                }
+                else
+                {
+                    f1[i] = Math.Max(f1[i - 1], f2[i - 1] + prices[i]);
+                    f2[i] = Math.Max(f2[i - 1], -1 * prices[i]);
+                }
+            }
+            return f1[_length - 1];
+        }
+    }
+}
--- algorithm-csharp/Program.cs
+++ algorithm-csharp/Program.cs
@@ -10,20 +10,16 @@ namespace algorithm_csharp
     {
         static void Main(string[] args)
         {
-            //int[] array = new[] {3,5,2,1,6,4};
-            ////Wiggle_Sort.MergeSort(array);
-            //Wiggle_Sort.QuickSort(array);
-            //Console.WriteLine(string.Join(",",array));
-
-            LogSystem log = new LogSystem();
-            log.Put(1, "2017:01:01:23:59:59");
-            log.Put(2, "2017:01:01:22:59:59");
-            log.Put(3, "2016:01:01:00:00:00");
-
-            Console.WriteLine(string.Join(",",log.Retrieve("2016:01:01:01:01:01", "2017:01:01:23:00:00", "Year")));
-            Console.WriteLine(string.Join(",", log.Retrieve("2016:01:01:01:01:01", "2017:01:01:23:00:00", "Hour")));
+            BestTimeToBuyAndSellStock test = new BestTimeToBuyAndSellStock();
+            Console.WriteLine(test.MaxProfit(new Int32[] { }));
+            Console.WriteLine(test.MaxProfit(null));
+            Console.WriteLine(test.MaxProfit(new Int32[] { 7, 1, 5, 3, 6, 4 }));
+            Console.WriteLine(test.MaxProfit(new Int32[] { 0, 1, 5, 3, 6, 9 }));
+            Console.WriteLine(test.MaxProfit(new Int32[] { 3, 2, 1, 3, 6, 9 }));
+            Console.WriteLine(test.MaxProfit(new Int32[] { 3, 2, 1, 9, 6, 0 }));
 
             Console.ReadKey();
+
         }
     }
 }
